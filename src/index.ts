b689e2fc5fc191840ee07020ebafc4b2e36c5377
sangam5756 <sangammunde3@gmail.com>
2025-04-06 11:49:10 +0530

    server working response

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/cache", cacheRoute);
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Redis Cache API");
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
